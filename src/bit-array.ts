5f651e6a7c828f5dc2259a149ba90fc5bbb9a4cb
Luke Gareth Ribchester <luke@luke.gr>
2025-01-29 15:23:20 +0100

    wip: update the `BitArray` class
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- src/bit-array.ts
+++ src/bit-array.ts
@@ -12,7 +12,13 @@ export class BitArray {
     }
 
     public setBit(word: number, offset: number): void {
+        // TODO: Setting a range = this mask -1 position back to the right.
         const mask = 1n << BigInt(offset);
         this.bitArray[word] = this.bitArray[word] | mask;
+    }
+
+    public clearBit(word: number, offset: number): void {
+        const mask = ~(1n << BigInt(offset));
+        this.bitArray[word] = this.bitArray[word] & mask;
     }
 }
