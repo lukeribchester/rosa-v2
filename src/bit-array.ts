06c4f9712a4d9b29167b8757cb3be05a4af315c9
Luke Gareth Ribchester <luke@luke.gr>
2025-01-29 15:16:22 +0100

    wip: create the `BitArray` class
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/bit-array.ts
@@ -0,0 +1,18 @@
+export class BitArray {
+    public readonly bitArray: BigUint64Array;
+
+    constructor(size: number) {
+        // Size (number of words in the array) = number of blocks / 64.
+        this.bitArray = new BigUint64Array(size / 64);
+    }
+
+    public checkBit(word: number, offset: number): boolean {
+        const mask = 1n << BigInt(offset);
+        return (this.bitArray[word] & mask) != BigInt(0);
+    }
+
+    public setBit(word: number, offset: number): void {
+        const mask = 1n << BigInt(offset);
+        this.bitArray[word] = this.bitArray[word] | mask;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import {Calendar} from "./calendar";
+import {BitArray} from "./bit-array";
 
 const calendar = new Calendar(
-    // 1st January 2025 to 31st December 2025.
     new Date('2025-01-01T00:00:00Z'),
-    new Date('2025-12-31T00:00:00Z'),
+    new Date('2025-01-02T08:00:00Z'),
     // 15 minutes in milliseconds.
     15 * (60 * 1000),
 );
+
+const bitArray = new BitArray(calendar.totalBlocks);
+
+const mask = 1n << BigInt(0);
+const mask2 = 1n << BigInt(1);
+const mask3 = (1n << 5n) - 1n;
+const isSet = (bitArray.bitArray[0] & mask) !== 0n;
+
+console.log((bitArray.bitArray[0] & mask) !== 0n);
+
+bitArray.bitArray[0] = bitArray.bitArray[0] | mask3;
+
+console.log(bitArray.bitArray);
+console.log((bitArray.bitArray[0] & mask) !== 0n);
+console.log(BigInt(1));
